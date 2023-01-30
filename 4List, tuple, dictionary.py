28bf516472c15766732c2fcef1df59981623ed46
Sherab Gyatso <123981843+gyatsosherab@users.noreply.github.com>
2023-01-30 19:54:30 +0100

    fourth lab list tuple and dictionary

---
+++ 4List, tuple, dictionary.py
@@ -0,0 +1,20 @@
+"""
+111-[PF]-Lab - List, tuple, dictionary
+"""
+#Defining a list
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#Accessing a List by position
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+"""
+Changing the values in a list
+"""
+
+myFruitList[2] = "orange"
+print(myFruitList)
