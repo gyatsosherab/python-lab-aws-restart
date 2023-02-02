b6b7b35873370f7ff1d36c11afac332f4b02c4bc
EC2 Default User <ec2-user@ip-10-16-10-254.ec2.internal>
2023-02-02 20:42:27 +0000

    15th Program

---
+++ 15System Administration.py
@@ -0,0 +1,31 @@
+"""
+Introducing System Administration with Python
+"""
+# import os
+
+# os.system("ls")
+
+#Exercise 2: Using subprocess.run
+import subprocess
+
+# subprocess.run(["ls"])
+
+#Exercise 3: Using subprocess.run with two arguments
+#subprocess.run(["ls","-l"])
+
+#Exercise 4: Using subprocess.run with three arguments
+# subprocess.run(["ls","-l","README.md"])
+
+#Exercise 5: Retrieving system information
+
+# command="uname"
+# commandArgument="-a"
+# print(f'Gathering system information with command: {command} {commandArgument}')
+# subprocess.run([command,commandArgument])
+
+#Exercise 6: Retrieving information about disk space
+
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
