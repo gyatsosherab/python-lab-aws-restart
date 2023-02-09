d55c85aae9a5b34d1879dcc9a7b0762e970994ca
EC2 Default User <ec2-user@ip-10-16-10-254.ec2.internal>
2023-02-09 17:56:28 +0000

    jsonFile

--- jsonFileHandler.py
+++ jsonFileHandler.py
@@ -3,7 +3,7 @@ import json
 def readJsonFile(fileName):
     data = ""
     try:
-        with open('files/insulin.json') as json_file:
+        with open('../files/insulin.json') as json_file:
             data = json.load(json_file)
     except IOError:
         print("Could not read file")
